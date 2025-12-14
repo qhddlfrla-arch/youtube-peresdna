c4fd4ea0a2bace343c0ba5e2a91ce07ed863b535
qhdlflrla <qhddlfrla@gmail.com>
2025-12-14 22:37:04 +0900

    index.css import 제거

--- index.tsx
+++ index.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
-import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
