1b972723fca3e1bfb1a49e847eee6f4ad2b4dc68
qhdlflrla <qhddlfrla@gmail.com>
2025-12-14 22:35:22 +0900

    index.tsx 수정 - 삭제된 페이지 import 제거

--- index.tsx
+++ index.tsx
@@ -3,11 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
-import GuidePage from './pages/GuidePage.tsx';
-import ApiGuidePage from './pages/ApiGuidePage.tsx';
-import AdminPage from './pages/AdminPage.tsx';
-import DownloadProgressPage from './pages/DownloadProgressPage.tsx';
-import DownloadPage from './pages/DownloadPage.tsx';
+import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -20,12 +16,8 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/guide" element={<GuidePage />} />
-        <Route path="/api-guide" element={<ApiGuidePage />} />
-        <Route path="/admin" element={<AdminPage />} />
-        <Route path="/download-progress" element={<DownloadProgressPage />} />
-        <Route path="/download" element={<DownloadPage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
+
